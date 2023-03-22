388ca0541242ae829285bf6f8e45e425557afebe
Dillon Hess <dhess268@gmail.com>
2023-03-22 14:51:55 -0400

    Add auth routes file and add dependencies

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,15 @@ const express = require('express');
 const app = express();
 
 const myAnimeListRouter = require('./src/routes/myAnimeList.route');
+const authRouter = require('./src/routes/auth.route');
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
 
 app.use('/myAnimeList', myAnimeListRouter);
+
+app.use('/auth', authRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => {
