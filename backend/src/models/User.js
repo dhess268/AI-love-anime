ef2f157670dce9b9f6e6d0a1b26924f2cc1c3eb3
Dillon Hess <dhess268@gmail.com>
2023-03-23 19:21:27 -0400

    Add user schema and password checking

---
+++ backend/src/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  avatar: { type: String, required: false },
+  hash: String,
+  salt: String,
+});
+
+UserSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+    .toString('hex');
+};
+
+UserSchema.methods.validPassword = function (password) {
+  // this.salt = crypto.randomBytes(16).toString('hex');
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+    .toString('hex');
+  return this.hash === hash;
+};
+
+module.exports = mongoose.model('User', UserSchema);
