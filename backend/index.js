792f69f8e4b6f6fbac968dbfaa6df984e160f570
Dillon Hess <dhess268@gmail.com>
2023-04-10 12:37:18 -0400

    serve file on production

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./src/configs/db');
 const User = require('./src/models/User');
 
@@ -50,6 +51,19 @@ app.use('/auth', authRouter);
 app.use('/api/user', requireSignin, userRouter);
 
 app.use('/api/gpt', requireSignin, gptRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 3001;
 
