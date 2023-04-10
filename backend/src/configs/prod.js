b84a2e2dafa8b8cf261256764f67ce6190ab42fd
Dillon Hess <dhess268@gmail.com>
2023-04-10 12:27:47 -0400

    Change prod variables

--- backend/src/configs/prod.js
+++ backend/src/configs/prod.js
@@ -1,4 +1,7 @@
 module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   TOKEN_SECRET: process.env.TOKEN_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: 86400 * 1,
+  OPENAI_KEY: process.env.OPENAI_KEY,
 };
